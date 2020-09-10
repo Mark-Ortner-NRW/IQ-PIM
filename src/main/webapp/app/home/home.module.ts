60fe615469ad50d154754a963f812c7ec25d85e0
Marko Ortner <marko.ortne@innoq.com>
2020-09-10 17:24:06 +0200

    Initial version of InnoqPim generated by JHipster-6.8.0

---
+++ src/main/webapp/app/home/home.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { InnoqPimSharedModule } from 'app/shared/shared.module';
+import { HOME_ROUTE } from './home.route';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  imports: [InnoqPimSharedModule, RouterModule.forChild([HOME_ROUTE])],
+  declarations: [HomeComponent]
+})
+export class InnoqPimHomeModule {}
---
+++ src/test/java/com/innoq/ArchTest.java
@@ -0,0 +1,29 @@
+package com.innoq;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.innoq");
+
+        noClasses()
+            .that()
+                .resideInAnyPackage("com.innoq.service..")
+            .or()
+                .resideInAnyPackage("com.innoq.repository..")
+            .should().dependOnClassesThat()
+                .resideInAnyPackage("..com.innoq.web..")
+        .because("Services and repositories should not depend on web layer")
+        .check(importedClasses);
+    }
+}
