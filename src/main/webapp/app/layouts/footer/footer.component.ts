aaa9a0e0b7713696bdade8a846f5afbaf593751e
Marko Ortner <marko.ortne@innoq.com>
2020-09-11 21:41:09 +0200

    befre upgrade

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -3,9 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
-<<<<<<< HEAD
   styleUrls: ['footer.scss']
-=======
->>>>>>> jhipster_upgrade
 })
 export class FooterComponent {}
