c9f522d575976caae5df64f817f133e7cf1c0d36
Marko Ortner <marko.ortne@innoq.com>
2020-09-15 10:08:07 +0200

    update

--- src/main/webapp/app/control-demo/page-one/page-one.component.ts
+++ src/main/webapp/app/control-demo/page-one/page-one.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ColorPickerModule } from 'primeng/colorpicker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TableModule } from 'primeng/table';
+import { IBrand } from 'app/shared/model/brand.model';
+import { BrandService } from 'app/entities/brand/brand.service';
+import { HttpHeaders, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'jhi-page-one',
@@ -11,9 +14,27 @@ import { TableModule } from 'primeng/table';
 export class PageOneComponent implements OnInit {
   message: string;
   color: any = '#24244c';
+  brands?: IBrand[] | any;
+  cols: any[] = [
+    { field: 'id', header: 'ID' },
+    { field: 'name', header: 'Name' },
+    { field: 'description ', header: 'Description' },
+    { field: 'createdAt', header: 'Created At' },
+  ];
 
-  constructor() {
+  constructor(protected brandService: BrandService) {
     this.message = 'PageOneComponent message';
   }
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.brandService.query({}).subscribe(
+      (res: HttpResponse<IBrand[]>) => this.onSuccess(res.body),
+      () => this.onError()
+    );
+  }
+
+  protected onSuccess(data: IBrand[] | null): void {
+    this.brands = data || [];
+  }
+
+  protected onError(): void {}
 }
